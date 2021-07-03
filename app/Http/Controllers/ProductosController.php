e887e31e7d3a1b7ec64e1abedf283c36e1256491
UN1T7D <ahorasoysegundo@mail.com>
2021-07-02 21:51:46 -0600

    first commit

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Productos;
+
+class ProductosController extends Controller
+{
+    public function index()
+    {
+        return Productos::paginate();
+    }
+
+    public function show($id)
+    {
+        return Productos::find($id);
+    }
+
+    public function create(Request $request)
+    {
+        $producto = new Productos();
+        $producto->nombre=  $request->input('nombre');
+        $producto->descripcion = $request->input('descripcion');
+        $producto->localidad = $request->input('localidad');
+        $producto->save();
+        return json_encode(['msg' => 'Agregado']);
+    }
+
+    public function destroy($id)
+    {
+        Productos::destroy($id);
+        return json_encode(['msg' => 'Eliminado']);
+    }
+
+    public function edit(Request $request , $id)
+    {
+        $nombre=  $request->input('nombre');
+        $descripcion = $request->input('descripcion');
+        $localidad = $request->input('localidad');
+
+        Productos::where('id' , $id)->update(
+            [
+                'nombre' => $nombre,
+                'descripcion' => $descripcion,
+                'localidad' => $localidad
+            ]
+        );
+
+        return json_encode(['msg' => 'Modificado']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductosController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/productos',[ProductosController::class, 'index']);
+Route::get('/productos/{id}',[ProductosController::class, 'show']);
+
+Route::post('/productos',[ProductosController::class, 'create']);
+Route::delete('/productos/{id}',[ProductosController::class, 'destroy']);
+Route::put('/productos/{id}',[ProductosController::class, 'edit']);
